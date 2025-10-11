b1b5683d3188bb4f3dee9bf9a47d591918b61a16
Penghui Yang <59365168+Paul-YPH@users.noreply.github.com>
2025-10-10 20:16:31 -0400

    Update

---
+++ .history/setup_20251010201136.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tacos",
+    version="0.0.1",
+    description="/",
+    author="/",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    python_requires=">=3.7",
+)
